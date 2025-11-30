a65ae7e752b0b68a31a62b50337770791bc77e74
Mohamed Jiyavutheen <jiyamalik1729@gmail.com>
2025-11-30 18:54:11 +0530

    initial coomit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import FeatureDestination from '../components/FeatureDestination'
+import Exclusiveoffers from '../components/Exclusiveoffers'
+import Testimonial from '../components/Testimonial'
+import Newsletter from '../components/Newsletter'
+
+const Home = () => {
+  return (
+    <>
+      <Hero/>
+      <FeatureDestination/>
+      <Exclusiveoffers/>
+      <Testimonial/>
+      <Newsletter/>
+      
+    </>
+  )
+}
+
+export default Home
